9da62e6379f25f49fad469f13f31452d036f2002
Binit Datta <binitdatta@Binit-Dattas-MacBook-Pro.local>
2018-11-13 10:10:57 -0600

    first commit

---
+++ src/main/java/com/rollingstone/RollingstoneEcommerceApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.rollingstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingstoneEcommerceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingstoneEcommerceApiGatewayApplication.class, args);
+	}
+}
